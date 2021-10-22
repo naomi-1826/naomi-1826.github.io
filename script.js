85e1add5eb01c9635fa272ee36e1c01d740ddbe8
naomi-1826 <92761049+naomi-1826@users.noreply.github.com>
2021-10-22 11:28:46 -1000

    added getUser() and edited userName()

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+var a
 function userName() {
-    userName = document.getElementById('name').value
+    //get value from input field and save as a
+    a = document.getElementById('name').value
     window.alert(userName)
+    //create LS key with value of "a" 
+    localStorage.setItem('uName', userName);
+    //automatically take user to page two.html 
+    location.href = "two.html";
+}
+
+function getUser() {
+    var b = localStorage.getItem('uName');
+    document.getElementById('placeholder').innerHTML = "Hello" + b; 
 }
 
 function variable() {
